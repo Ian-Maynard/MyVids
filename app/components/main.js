7ac31af6dd75d01b8296b5e949a68041c349408e
Ian-Maynard <maynard.ian@gmail.com>
2017-08-23 16:19:01 -0400

    Multiple changes

---
+++ app/components/main.js
@@ -0,0 +1,12 @@
+// React
+var React = require("react");
+var Link = require(react-router).link;
+
+var  Main = React.crateClass  ({
+    render: function() {
+        return (
+            <div className="main-container">
+                <div className="container">
+                
+                </div>
+            </div>
